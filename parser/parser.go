ead31b8ba1e40ce84b30ee6b1f77093918e8333b
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-06-28 01:28:44 +0530

    bool and grouped expression parsing

--- parser/parser.go
+++ parser/parser.go
@@ -56,6 +56,9 @@ func New(l *lexer.Lexer) *Parser {
     p.registerPrefix(token.INT, p.parserIntegerLiteral)
     p.registerPrefix(token.BANG, p.parsePrefixExpression)
     p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+    p.registerPrefix(token.TRUE, p.parseBoolean)
+    p.registerPrefix(token.FALSE, p.parseBoolean)
+    p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
     p.infixParseFns = make(map [token.TokenType]infixParseFn)
     p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -151,6 +154,13 @@ func (p *Parser) parserIntegerLiteral() ast.Expression {
     return literal
 }
 
+func (p *Parser) parseBoolean() ast.Expression {
+    return &ast.Boolean{
+        Token: p.currentToken,
+        Value: p.currentTokenIs(token.TRUE),
+    }
+}
+
 // ------------------------------------------------------
 //                  STATEMENT PARSERS
 // ------------------------------------------------------
@@ -200,6 +210,22 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
     }
 
     return stmt
+}
+
+// ------------------------------------------------------
+//                  EXPRESSION PARSERS
+// ------------------------------------------------------
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+    p.nextToken()
+
+    exp := p.parseExpression(LOWEST)
+
+    if !p.expectPeek(token.RPAREN) {
+        return nil
+    }
+
+    return exp
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
