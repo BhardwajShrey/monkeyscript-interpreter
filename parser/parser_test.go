d9a26840b81db0bceacd7d696ac3e5e1d33e0253
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-06-28 00:47:44 +0530

    some functions added to parser_test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -223,6 +223,63 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
     return true
 }
 
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+    ident, ok := exp.(*ast.Identifier)
+    if !ok {
+        t.Errorf("exp is not *ast.Identifier. Got %T instead.", exp)
+        return false
+    }
+
+    if ident.Value != value {
+        t.Errorf("ident.Value not equal to %s. Got %s instead,", value, ident.Value)
+        return false
+    }
+
+    if ident.TokenLiteral() != value {
+        t.Errorf("ident.TokenLiteral not equal to %s. Got %s instead,", value, ident.Value)
+        return false
+    }
+
+    return true
+}
+
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+    switch v := expected.(type) {
+        case int:
+            return testIntegerLiteral(t, exp, int64(v))
+        case int64:
+            return testIntegerLiteral(t, exp, v)
+        case string:
+            return testIdentifier(t, exp, v)
+    }
+    t.Errorf("Type of exp not handled. Got %T.", exp)
+    return false
+}
+
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
+    opExp, ok := exp.(*ast.InfixExpression)
+    
+    if !ok {
+        t.Errorf("exp is not ast.OperatorExpression. got=%T(%s)", exp, exp)
+        return false
+    }
+
+    if !testLiteralExpression(t, opExp.Left, left) {
+        return false
+    }
+
+    if opExp.Operator != operator {
+        t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
+        return false
+    }
+
+    if !testLiteralExpression(t, opExp.Right, right) {
+        return false
+    }
+    
+    return true
+}
+
 func TestInfixParsingExpressions (t *testing.T) {
     infixTests := [] struct {
         input string
