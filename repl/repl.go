6e8ad4b01cea96eccb271bb3847d5b7a5d0bbdd9
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-07-04 00:31:58 +0530

    integer literal evaluation and addition of E to repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+    "monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -35,8 +36,11 @@ func Start(in io.Reader, out io.Writer) {
         //     fmt.Printf("%+v\n", tok)
         // }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
     }
 }
 
