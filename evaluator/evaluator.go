75438ff0182a034c147cbcec68e8daae74628ed8
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-07-05 00:56:17 +0530

    integer literals and null added to Eval()

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,13 @@ import (
     "monkey/object"
 )
 
+// no need to create new instances of true and false every time if we can reference them
+var (
+    NULL = &object.Null{}
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
         // STATEMENTS
@@ -15,9 +22,18 @@ func Eval(node ast.Node) object.Object {
         // EXPRESSIONS
         case *ast.IntegerLiteral:
             return &object.Integer{Value: node.Value}
+        case *ast.Boolean:
+            return boolToBoolean(node.Value)
     }
 
     return nil
+}
+
+func boolToBoolean(val bool) *object.Boolean {
+    if val {
+        return TRUE
+    }
+    return FALSE
 }
 
 func evalStatements(stmts [] ast.Statement) object.Object {
