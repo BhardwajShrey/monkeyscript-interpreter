5aad0f5a57f08e7953c99f536084c0a73c9e473d
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-07-07 01:04:12 +0530

    prefix expression evaluation for - and bang

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -22,6 +22,9 @@ func Eval(node ast.Node) object.Object {
         // EXPRESSIONS
         case *ast.IntegerLiteral:
             return &object.Integer{Value: node.Value}
+        case *ast.PrefixExpression:
+            right := Eval(node.Right)
+            return evalPrefixExpression(node.Operator, right)
         case *ast.Boolean:
             return boolToBoolean(node.Value)
     }
@@ -44,4 +47,37 @@ func evalStatements(stmts [] ast.Statement) object.Object {
     }
 
     return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+        case "!":
+            return evalBangOperatorExpression(right)
+        case "-":
+            return evalMinusPrefixOperatorExpression(right)
+        default:
+            return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+        case TRUE:
+            return FALSE
+        case FALSE:
+            return TRUE
+        case NULL:
+            return TRUE
+        default:
+            return FALSE
+    }
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+    if right.Type() != object.INTEGER_OBJ {
+        return NULL
+    }
+
+    value := right.(*object.Integer).Value
+    return &object.Integer{Value: -value}
 }
