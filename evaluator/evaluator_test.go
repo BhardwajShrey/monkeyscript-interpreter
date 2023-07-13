c0fb68724f7fc3d47050764fc43097f81a6666b4
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-07-14 01:07:47 +0530

    return statement eval

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -54,7 +54,7 @@ func TestEvalIntegerExpression(t *testing.T) {
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
     result, ok := obj.(*object.Integer)
     if !ok {
-        t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+        t.Errorf("object is not Integer. got=%T (%+v). Expected value is %d.", obj, obj, expected)
         return false 
     }
 
@@ -155,5 +155,27 @@ func TestIfElseExpressions(t *testing.T) {
         } else {
             testNullObject(t, evaluated)
         }
+    }
+}
+
+func TestReturnStatements(t *testing.T) {
+    tests := [] struct {
+        input string
+        expected int64
+    } {
+        {"return 10;", 10},
+        {"return 10; 9;", 10},
+        {"return 2 * 5; 9;", 10},
+        {"9; return 5 * 2; 9;", 10},
+        {` if (10 > 1) {
+            if (10 > 1) {
+                return 10;
+            }
+            return 1; }`, 10},
+    }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        testIntegerObject(t, evaluated, tt.expected)
     }
 }
