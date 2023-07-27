b0e21479a2f09f5198a47b4919e1ab5517b870e7
Shrey Bhardwaj <bhardwajshrey10@gmail.com>
2023-07-27 18:00:31 +0530

    index operator for hashmap

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -101,6 +101,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
                 return elems[0]
             }
             return &object.Array{Elements: elems}
+        case *ast.HashLiteral:
+            return evalHashLiteral(node, env)
     }
 
     return nil
@@ -291,6 +293,32 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
     return newError("identifier not found: " + node.Value)
 }
 
+func evalHashLiteral(node *ast.HashLiteral, env *object.Environment) object.Object {
+    pairs := make(map[object.HashKey]object.HashPair)
+
+    for keyNode, valueNode := range node.Pairs {
+        key := Eval(keyNode, env)
+        if isError(key) {
+            return key
+        }
+
+        hashKey, ok := key.(object.Hashable)
+        if !ok {
+            return newError("unusable as hash key: %s", key.Type())
+        }
+
+        value := Eval(valueNode, env)
+        if isError(value) {
+            return value
+        }
+
+        hashed := hashKey.HashKey()
+        pairs[hashed] = object.HashPair{Key: key, Value: value}
+    }
+
+    return &object.Hash{Pairs: pairs}
+}
+
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
     var result []object.Object
 
@@ -309,6 +337,8 @@ func evalIndexExpression(left, index object.Object) object.Object {
     switch {
     case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
         return evalArrayIndexExpression(left, index)
+    case left.Type() == object.HASH_OBJ:
+        return evalHashIndexExpression(left, index)
     default:
         return newError("index operator not supported: %s", left.Type())
     }
@@ -325,6 +355,22 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
     }
 
     return arrayObject.Elements[idx]
+}
+
+func evalHashIndexExpression(hash, index object.Object) object.Object {
+    hashObject := hash.(*object.Hash)
+
+    key, ok := index.(object.Hashable)
+    if !ok {
+        return newError("unusable as hash key: %s", index.Type())
+    }
+
+    pair, ok := hashObject.Pairs[key.HashKey()]
+    if !ok {
+        return NULL
+    }
+
+    return pair.Value
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+    "hash/fnv"
 	"monkey/ast"
 	"strings"
 )
@@ -21,11 +22,16 @@ const (
     STRING_OBJ = "STRING"
     BUILTIN_OBJ = "BUILTIN"
     ARRAY_OBJ = "ARRAY"
+    HASH_OBJ = "HASH"
 )
 
 type Object interface {
     Type() ObjectType
     Inspect() string
+}
+
+type Hashable interface {
+    HashKey() HashKey
 }
 
 type Error struct {
@@ -52,6 +58,13 @@ func (i *Integer) Inspect() string {
     return fmt.Sprintf("%d", i.Value)
 }
 
+func (i *Integer) HashKey() HashKey {
+    return HashKey {
+        Type: i.Type(),
+        Value: uint64(i.Value),
+    }
+}
+
 type Boolean struct {
     Value bool
 }
@@ -64,6 +77,21 @@ func (b *Boolean) Inspect() string {
     return fmt.Sprintf("%t", b.Value)
 }
 
+func (b *Boolean) HashKey() HashKey {
+    var value uint64
+
+    if b.Value {
+        value = 1
+    } else {
+        value = 0
+    }
+
+    return HashKey {
+        Type: b.Type(),
+        Value: value,
+    }
+}
+
 type String struct {
     Value string
 }
@@ -74,6 +102,16 @@ func (s *String) Type() ObjectType {
 
 func (s *String) Inspect() string {
     return s.Value
+}
+
+func (s *String) HashKey() HashKey {
+    h := fnv.New64a()
+    h.Write([]byte(s.Value))
+    
+    return HashKey {
+        Type: s.Type(),
+        Value: h.Sum64(),
+    }
 }
 
 type Null struct {}
@@ -157,6 +195,39 @@ func (ao *Array) Inspect() string {
     out.WriteString("[")
     out.WriteString(strings.Join(elems, ", "))
     out.WriteString("]")
+
+    return out.String()
+}
+
+type HashKey struct {
+    Type ObjectType
+    Value uint64
+}
+
+type HashPair struct {
+    Key Object
+    Value Object
+}
+
+type Hash struct {
+    Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType {
+    return HASH_OBJ
+}
+
+func (h *Hash) Inspect() string {
+    var out bytes.Buffer
+
+    pairs := []string{}
+    for _, pair := range h.Pairs {
+        pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+    }
+    
+    out.WriteString("{")
+    out.WriteString(strings.Join(pairs, ", "))
+    out.WriteString("}")
 
     return out.String()
 }
